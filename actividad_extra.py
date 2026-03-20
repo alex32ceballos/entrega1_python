bc6a9cb0e8dc1d33404fa74c4112f95e43b80744
alex3209 <alexleonel2006@gmail.com>
2026-03-20 11:55:44 -0300

    cree estructura y mensaje de interfazç

--- actividad_extra.py
+++ actividad_extra.py
@@ -1,1 +1,15 @@
-print("hola")
+tabla = { #agregue algunos equipos a la tabla
+    "Platense":2,
+    "Estudiantes":6,
+    "Boca":8
+}
+
+def menuOpciones():
+    print("¡BIENVENIDO A LA TABLA DE POSICIONES!\nIngrese una de las siguientes opciones: \n")
+    print("0. Agregar un equipo al torneo.")
+    print("1. Registrar partido")
+    print("2. Mostrar tabla de posiciones(menor a mayor)")
+    print("3. Eliminar un equipo del torneo")
+    print("4. Salir del programa")
+    
+menuOpciones()
