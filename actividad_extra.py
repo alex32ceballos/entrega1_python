d1adfc00e330151983eb54ae100f66889de93756
alex3209 <alexleonel2006@gmail.com>
2026-03-20 14:33:48 -0300

    funcion actualizar partido y mostrar tabla

--- actividad_extra.py
+++ actividad_extra.py
@@ -11,11 +11,65 @@ def eliminarEquipo(equipo): #funcion para eliminar equipo
     else:
         print("el equipo ingresado no se encuentra en la tabla")
         
-def agregarEquipo(equipo):
+        
+def agregarEquipo(equipo): #agrego un equipo
     if (not equipo in tabla):
         tabla[equipo]=0
     else:
         print('No se puede agregar el equipo porque ya se encuentra')
+        
+        
+def partidoRegistrar(local,visitante, golesLocal, golesVisitante):
+    if (local in tabla) and (visitante in tabla):
+        if (golesLocal > golesVisitante):
+            tabla[local]+=3
+        elif (golesVisitante > golesLocal):
+            tabla[visitante]+=3
+        else:
+            tabla[local]+=1
+            tabla[visitante]+=1
+    else:
+        print("No se puede registrar el partido. Uno o ambos equipos no se encuentran en la tabla")
+
+
+def marcadorEsNumero(marcador,local,visitante):
+    if (marcador[0].isdigit() and marcador[1].isdigit()):
+        golesLocal=int(marcador[0])
+        golesVisitante=int(marcador[1])
+        partidoRegistrar(local,visitante,golesLocal,golesVisitante)
+    else:
+        print('formato de marcador invalido')
+        
+
+def partidoActualizar(): #actualiza la tabla segun el partido
+    local = input("ingresar equipo local: ")
+    visitante = input("ingresar equipo visitante: ")
+    marcador = input("ingresar marcador: ")
+    if marcador.count("-") == 1:
+        marcador = marcador.split("-")
+        marcadorEsNumero(marcador,local,visitante)
+    else:
+        print('formato de marcador invalido')
+  
+  
+def ordenarTabla(): #creo una lista de listas 
+    tablaOrdenada = []
+    for clave,valor in tabla.items():
+        datos = []
+        datos.append(valor)
+        datos.append(clave)
+        tablaOrdenada.append(datos)
+    tablaOrdenada.sort(reverse=True)
+    return tablaOrdenada
+
+def mostrarTabla():
+    tablaOrdenada = ordenarTabla()
+    print("--------------------------------------------------")
+    print("posicion   |      equipo      |    puntaje")
+    contador = 1
+    for subLista in tablaOrdenada:
+        print(f"{contador}          |      {subLista[1]}      |    {subLista[0]}")
+        contador+=1
         
 
 def menuOpciones():
